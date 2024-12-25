e984baa13f1b16ef9e90ab54d359f36ef840bccb
peyn-dev <ariya.peyn01@gmail.com>
2024-12-25 11:35:18 +0800

    Registration Form

--- src/app/core/auth/register/register.component.ts
+++ src/app/core/auth/register/register.component.ts
@@ -1,8 +1,9 @@
 import {Component, inject} from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators, AbstractControl} from '@angular/forms';
 import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {MatButton} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import {NgIf} from '@angular/common';
 
 @Component({
@@ -15,22 +16,45 @@ import {NgIf} from '@angular/common';
     NgIf,
     MatError,
     MatLabel,
+    MatCardModule
   ],
   templateUrl: './register.component.html',
   standalone: true,
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  fb = inject(FormBuilder);
-  registerForm = this.fb.nonNullable.group({
-      username: ['', [Validators.required, Validators.minLength(4)]],
-      password: ['', [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$')]],
-      confirmPassword: ['', Validators.required, this.passwordMatchValidator]
-    });
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+    },
+    {
+      validator: this.passwordMatchValidator
+    }
+  );
+  console.log(this.registerForm)
+  }
 
-  passwordMatchValidator(form: FormGroup) {
-    return form.get('password')?.value === form.get('confirmPassword')?.value ? null : {mismatch: true};
+  passwordMatchValidator(formGroup: FormGroup) {
+
+    const password = formGroup.get('password')?.value;
+
+    const confirmPassword = formGroup.get('confirmPassword')?.value;
+
+    return password === confirmPassword ? null : { passwordsMismatch: true };
+
+  }
+
+  register() {
+    if (this.registerForm.valid) {
+      console.log('Registration successful!', this.registerForm.value);
+    } else {
+      console.error('Form is invalid');
+    }
   }
 
   onSubmit() {
